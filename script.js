3065fdc43287fd896d956b6e2fac0f7f1eaeda73
L3ynaT <leynatran1221@gmail.com>
2025-07-04 23:58:06 -0700

    added buttons and connected to javascript

---
+++ script.js
@@ -0,0 +1,24 @@
+var button2022 = $(".event2022");
+var button2023 = $(".event2023");
+var button2024 = $(".event2024");
+var button2025 = $(".event2025");
+
+var kwkEvent = $(".kwk");
+var pythonEvent = $(".python");
+var clubEvent = $(".club");
+var javaEvent = $(".java");
+
+button2022.on("click", kwkShow);
+
+function kwkShow() {
+  kwkEvent.append(
+    `<p><a href="https://www.kodewithklossy.com/camp">KWK Web Dev Camp</a> Scholar</p>`
+  );
+}
+
+//https://images.unsplash.com/vector-1738590593450-647695dbf9d0?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
+//https://images.unsplash.com/vector-1739803879927-630520aa7384?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
+//https://images.unsplash.com/vector-1748272331105-e604244e3386?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
+//https://images.unsplash.com/photo-1501594907352-04cda38ebc29?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
+//https://images.unsplash.com/photo-1617791160536-598cf32026fb?q=80&w=1064&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
+//https://images.unsplash.com/photo-1674027444485-cec3da58eef4?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
